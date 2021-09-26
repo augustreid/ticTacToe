e2a16bd495d9c02dfbe34b0b1f42ca1d60b56ef2
August Reid <c.august.reid@gmail.com>
2021-09-26 16:37:11 -0600

    Add turn declaration

--- game.js
+++ game.js
@@ -50,60 +50,16 @@ class Game {
   };
 
   resetBoard() {
-    this.a1 = 0;
-    this.a2 = 0;
-    this.a3 = 0;
-    this.b1 = 0;
-    this.b2 = 0;
-    this.b3 = 0;
-    this.c1 = 0;
-    this.c2 = 0;
-    this.c3 = 0;
+    this.a1 = "empty";
+    this.a2 = "empty";
+    this.a3 = "empty";
+    this.b1 = "empty";
+    this.b2 = "empty";
+    this.b3 = "empty";
+    this.c1 = "empty";
+    this.c2 = "empty";
+    this.c3 = "empty";
     this.win = false;
     this.draw = false;
   };
 };
-
-// module.exports = Game;
-
-// var trial = new Game();
-//
-// trial.playerOneTurn("a1");
-//
-// trial.playerTwoTurn("b2");
-//
-// trial.playerOneTurn("c2");
-//
-// trial.playerTwoTurn("b3");
-//
-// trial.playerOneTurn("c3");
-//
-// trial.playerTwoTurn("b1");
-//
-// trial.playerOneTurn("a2");
-//
-// trial.playerTwoTurn("c1");
-//
-// trial.playerOneTurn("a3");
-//
-// trial.checkForWin();
-// console.log(trial);
-
-//Two instances of Player;
-//9 squares with three states;
-//who's turn is it, 1 or 2.
-
-// If Win conditions:
-//1a, 1b, 1c
-//2a, 2b 2c
-//3a 3b 3c
-//1a 2a 3a
-//1b 2b 3b
-//1c 2c 3c
-//1a 2b 3c
-//3a 2b 1c
-//check for win method
-// use === to check for three squares the same.
-//have an isWon property
-//have a isDraw property
-//change square value methods for each player. At the end, the who's turn is it property changes.
--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// const Game = require("./game.js");
-// const Player = require("./player.js");
-
-
 var board = document.querySelector("#board")
 var boardA1 = document.querySelector("#a1")
 var boardA2 = document.querySelector("#a2")
@@ -12,10 +8,13 @@ var boardB3 = document.querySelector("#b3")
 var boardC1 = document.querySelector("#c1")
 var boardC2 = document.querySelector("#c2")
 var boardC3 = document.querySelector("#c3")
+var gameTitle = document.querySelector("#gameTitle")
 
 var game = new Game();
 var squares = []
 var beesAndSuns = [boardA1, boardA2, boardA3, boardB1, boardB2, boardB3, boardC1, boardC2, boardC3]
+
+
 //event listeners:
 board.addEventListener("click", makeAMove)
 
@@ -28,7 +27,6 @@ function makeAMove() {
   playTurn(event);
   game.checkForWin();
   render();
-  console.log(game.win);
 }
 
 
@@ -42,6 +40,15 @@ function playTurn(event) {
   squares = [game.a1, game.a2, game.a3, game.b1, game.b2, game.b3, game.c1, game.c2, game.c3]
 };
 
+function declareWinner() {
+  if (game.win && game.whosTurn === 2) {
+    gameTitle.innerText = "Player One wins!"
+  } else if (game.win && game.whosTurn === 1) {
+    gameTitle.innerText = "Player two wins!"
+  } else if (game.draw) {
+    gameTitle.innerText = "It's a draw!"
+  }
+}
 
 function render() {
   event.preventDefault();
@@ -52,4 +59,13 @@ function render() {
       beesAndSuns[i].innerHTML = "&#128029";
     };
   };
+  nameTurn();
 };
+
+function nameTurn() {
+  if (game.whosTurn === 1) {
+    gameTitle.innerHTML = "Player One's Turn &#127803"
+  } else {
+    gameTitle.innerHTML = "Player Two's Turn &#128029"
+  }
+}
