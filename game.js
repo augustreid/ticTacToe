244e8499c842868e846ae1161e478c27eaceb5f9
August Reid <c.august.reid@gmail.com>
2021-09-28 10:30:59 -0600

    Add turn counter

--- game.js
+++ game.js
@@ -14,19 +14,20 @@ class Game {
     this.whosTurn = 1;
     this.win = false;
     this.draw = false;
+    this.totalTurns = 0;
   };
 
   playerOneTurn(square) {
     if (this[square] === 0) {
       this[square] = 1;
-      // this.whosTurn = 2;
+      this.totalTurns++;
   };
 };
 
   playerTwoTurn(square) {
     if (this[square] === 0) {
       this[square] = 2;
-      // this.whosTurn = 1;
+      this.totalTurns++;
     };
   };
 
@@ -48,27 +49,9 @@ class Game {
       ((this.a3 === this.b3) && (this.b3 === this.c3) && (this.c3 === this.whosTurn)) ||
       ((this.a1 === this.b2) && (this.b2 === this.c3) && (this.c3 === this.whosTurn)) ||
       ((this.a3 === this.b2) && (this.b2 === this.c1) && (this.c1 === this.whosTurn))) {
-      // (this.a1 + this.a2 + this.a3 === 3) ||
-      // (this.a1 + this.a2 + this.a3 === 6) ||
-      // (this.b1 + this.b2 + this.b3 === 3) ||
-      // (this.b1 + this.b2 + this.b3 === 6) ||
-      // (this.c1 + this.c2 + this.c3 === 3) ||
-      // (this.c1 + this.c2 + this.c3 === 6) ||
-      // (this.a1 + this.b1 + this.c1 === 3) ||
-      // (this.a1 + this.b1 + this.c1 === 6) ||
-      // (this.a2 + this.b2 + this.c2 === 3) ||
-      // (this.a2 + this.b2 + this.c2 === 6) ||
-      // (this.a3 + this.b3 + this.c3 === 3) ||
-      // (this.a3 + this.b3 + this.c3 === 6) ||
-      // (this.a1 + this.b2 + this.c3 === 3) ||
-      // (this.a1 + this.b2 + this.c3 === 6) ||
-      // (this.a3 + this.b2 + this.c1 === 3) ||
-      // (this.a3 + this.b2 + this.c1 === 6)) {
       this.win = true;
   } else if
-    (this.a1 && this.a2 && this.a3 && this.b1 &&
-    this.b2 && this.b3 && this.c1 && this.c2 &&
-    this.c3) {
+    (this.totalTurns === 9) {
       this.draw = true;
     };
     this.changeTurn();
@@ -86,5 +69,6 @@ class Game {
     this.c3 = 0;
     this.win = false;
     this.draw = false;
+    this.totalTurns = 0;
   };
 };
